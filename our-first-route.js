75433e30f5c7e68c1798ab3fc6bf8281684aed72
2b03.2012014.lumyuliang <2b03.2012014.lumyuliang@gmail.com>
2023-07-31 12:06:44 +0800

    added crud and verified works

--- our-first-route.js
+++ our-first-route.js
@@ -27,4 +27,78 @@ export default async function routes(fastify, options) {
     // https://stackoverflow.com/questions/76207360/why-does-fastify-send-a-response-and-doesnt-wait-for-my-response
     return reply
   })
+
+  // Define schema for new todo items
+  const todoItem_schema = {
+    type: 'object',
+    required: ['todo_item'],
+    properties: {
+      'todo_item': { type: 'string' }
+    }
+  }
+
+  const schema = {
+    body: todoItem_schema
+  }
+
+  // Add new todo item
+  fastify.post('/todoitems', { schema }, async (request, reply) => {
+    fastify.mysql.query(
+      'INSERT INTO todo_items(todo_desc) VALUES (?)',
+      [request.body['todo_item']], // we can use the `request.body` object to get the data sent by the client
+      function onResult(err, result) {
+        if (err) {
+          fastify.log.error(err)
+        }
+        
+
+        // console.log(result)
+        reply.status(201).send(result)
+      }
+    )
+
+    return reply
+  })
+
+  // Delete specific todo item
+  fastify.delete('/todoitems/:id', async (request, reply) => {
+    fastify.mysql.query(
+      'DELETE FROM todo_items WHERE id=?',
+      [request.params.id],
+      function onResult(err, result) {
+        if (err) {
+          fastify.log.error(err)
+        }
+
+        // console.log(result)
+        reply.send(result)
+      }
+    )
+
+    // Issue with nodeJS streams require returning reply
+    // https://stackoverflow.com/questions/76207360/why-does-fastify-send-a-response-and-doesnt-wait-for-my-response
+    return reply
+  })
+
+
+  fastify.put('/todoitems/:id', { schema }, async (request, reply) => {
+    fastify.mysql.query(
+      'UPDATE todo_items SET todo_desc=? WHERE id=?',
+      [request.body['todo_item'], request.params.id],
+      function onResult(err, result) {
+        if (err) {
+          fastify.log.error(err)
+        }
+
+        // console.log(result)
+        reply.send(result)
+      }
+    )
+
+    // Issue with nodeJS streams require returning reply
+    // https://stackoverflow.com/questions/76207360/why-does-fastify-send-a-response-and-doesnt-wait-for-my-response
+    return reply
+  })
+
+
 }
