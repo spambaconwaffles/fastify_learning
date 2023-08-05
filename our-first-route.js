cca2e8f92a76b2cb3205fdaf795c841b4c10146e
2b03.2012014.lumyuliang <2b03.2012014.lumyuliang@gmail.com>
2023-08-05 16:18:23 +0800

    added date validation for post and put

--- our-first-route.js
+++ our-first-route.js
@@ -83,13 +83,42 @@ export default async function routes(fastify, options) {
 
   // Add new todo item
   fastify.post('/todoitems', { schema }, async (request, reply) => {
+    // The schema turns null values to empty string because the schema
+    // has type coercion by default.
+    if (request.body['doneBy'] !== '') {
+      // Get current date
+      // The current time is removed before the date is passed to Date() to ensure
+      // that time is not a factor when comparing dates
+      const currDate = new Date(new Date().toISOString().split('T')[0])
+
+      const doneByDateObj = new Date(request.body['doneBy'])
+
+      // Check if date received is valid date (that means no such thing like "2022-10-34")
+      if (doneByDateObj.toString() === 'Invalid Date') {
+        fastify.log.error('Invalid date provided')
+        reply.statusCode = 400
+
+        reply.send({ error: 'Invalid date provided' })
+        return reply
+      }
+
+      // Validate doneBy to not allow dates before currDate
+      if (doneByDateObj < currDate) {
+        fastify.log.error('Date provided is before current date')
+        reply.statusCode = 400
+
+        reply.send({ error: 'Date provided is before current date' })
+        return reply
+      }
+    }
+
     fastify.mysql.query(
       'INSERT INTO todo_items(todo_desc, doneBy) VALUES (?, ?)',
       [request.body['todo_desc'], request.body['doneBy']], // we can use the `request.body` object to get the data sent by the client
       function onResult(err, result) {
         if (err) {
           fastify.log.error(err)
-          reply.statusCode = 400
+          reply.statusCode = 500
           reply.send(err)
         }
 
@@ -105,13 +134,42 @@ export default async function routes(fastify, options) {
 
   // Update todo item by id
   fastify.put('/todoitems/:id', { schema }, async (request, reply) => {
+    // The schema turns null values to empty string because the schema
+    // has type coercion by default.
+    if (request.body['doneBy'] !== '') {
+      // Get current date
+      // The current time is removed before the date is passed to Date() to ensure
+      // that time is not a factor when comparing dates
+      const currDate = new Date(new Date().toISOString().split('T')[0])
+
+      const doneByDateObj = new Date(request.body['doneBy'])
+
+      // Check if date received is valid date (that means no such thing like "2022-10-34")
+      if (doneByDateObj.toString() === 'Invalid Date') {
+        fastify.log.error('Invalid date provided')
+        reply.statusCode = 400
+
+        reply.send({ error: 'Invalid date provided' })
+        return reply
+      }
+
+      // Validate doneBy to not allow dates before currDate
+      if (doneByDateObj < currDate) {
+        fastify.log.error('Date provided is before current date')
+        reply.statusCode = 400
+
+        reply.send({ error: 'Date provided is before current date' })
+        return reply
+      }
+    }
+
     fastify.mysql.query(
       'UPDATE todo_items SET todo_desc=?, doneBy=? WHERE id=?',
       [request.body['todo_desc'], request.body['doneBy'], request.params.id],
       function onResult(err, result) {
         if (err) {
           fastify.log.error(err)
-          reply.statusCode = 400
+          reply.statusCode = 500
           reply.send(err)
         }
 
